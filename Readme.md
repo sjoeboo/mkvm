c674e78f021703034443698f2b23a6ff6f92fafc
Matthew Nicholson <matthew.a.nicholson@gmail.com>
2016-02-10 14:30:17 -0500

    mostly working now

--- Readme.md
+++ Readme.md
@@ -24,4 +24,125 @@ Setup:
     :password: 'changeme'
 ```
 substituting your info. Ideally make this file only accessible by your user. Sadly, we cannot YET pass this into foreman/prompt for it if its missing.
-* 
+
+* *If you wish to omit your password from this file, you should always call mkvm with ```-p``` to have it prompt you for a password and always pass that to all the hammer cli commands*
+
+Usage/Configuration:
+--------------------
+
+```mkvm``` can be told how to make a VM in a few ways, via built in defaults, config files, and command line arguments. These are merged in in order, so it is possible to override defaults with config files and cli arguments and also use cli arguments to override config file values.
+
+Built in defaults will mostly NOT work. Many values (like what Folder to place a vm into, what partition-table to use, etc) are so site-specific that they are left undefined/nil on purpose.
+
+Config files would be the preferred way of using ```mkvm```, as you can have multiple config files representing different setups of vms and simple refer to them over and and over. Config files are simply yaml documents with key/value pairs representing the config options.  An example configuration may look like:
+
+```
+---
+arch: 1
+compute_resource: 1
+ptable: 55
+media: 1
+os: 1
+host_group: 1
+cpus: 1
+memory: 512
+managed: false
+guest_type: 'rhel7_64Guest'
+path: '/Datacenters/MYDC/vm/Some Folder/Dev'
+cluster: 'MY Cluster 01'
+nic_type: 'VirtualE1000'
+nic_network: 'VLAN01'
+volume_datastore:
+  - 'LUN1'
+  - 'LUN2'
+volume_size: 10
+start: 1
+```
+
+Note that config files can contain all or only some of the options for creating a vm, since options will be merged. The full list of options/defaults are:
+
+
+```
+default_options = {
+  :verbose => false,
+  :debug => false,
+  :test => false,
+  :passwd => false,
+  :arch => '1',
+  :host_group => 1,
+  :media => nil,
+  :os => 1,
+  :ptable => 1,
+  :compute_resource => 1,
+  :cpus => 1, #-c
+  :memory => 512,
+  :guest_type => 'rhel7_64Guest',
+  :path => nil,
+  :cluster => nil,
+  :nic_type => 'VirtualE1000',
+  :nic_network => nil,
+  :nic_name => 'eth0',
+  :nic_managed => false,
+  :nic_ip => nil,
+  :nic_mac => nil,
+  :volume_datastore => nil,
+  :volume_size => 10,
+  :quick => false,
+  :start => true,
+  :build => true,
+}
+```
+
+Finally, all of these options are exposed via cli arguments/options:
+```mkvm.rb --help```
+
+```
+Usage: mkvm.rb [options] NAME
+    -v, --verbose                    Run verbosely [false]
+    -d, --debug                      Pass --debug to all hammer commands [false]
+    -f, --configfile PATH            Set config file (yaml, values will merge with defaults/cli options [nil]
+    -t, --test                       Test, will not create vm, just output resulting hammer command[false]
+    -p, --passwd                     Prompt for password to pass to hammer with each call [false]
+    -a, --arch ARCH                  Architecture ID [1]
+    -c, --cpus CPUS                  CPUS [1]
+    -b, --cluster CLUSTER            VMware Cluster [nil]
+    -e, --path PATH                  Folder Path for VM [nil]
+    -g, --group HOST_GROUP           Host GroupID [1]
+    -i, --media MEDIA                Installation MediaID [nil]
+    -m, --memory MEMORY              Memory(RAM) in MB [512]
+        --nic-name ETH0              NIC name [eth0]
+        --nic-network VLAN64         Network/VLAN for NIC [VLAN01]
+        --nic-type VirtualE1000      NIC Hardware Type [VirtualE1000]
+        --nic-ip 192.168.1.10        NIC IP [nil]
+        --nic-mac 00:32:32:32:32     NIC Mac [random]
+        --vol-datastore DATASTORE    Datastore/LUN [nil]
+        --vol-size SIZE_GB           Volume size in GB [10]
+    -k, --guest_type TYPE            Guest Type ['Red Hat Enterprise  Linux 7 (64-bit)']
+    -l, --nic_managed                Set NIC to be foreman managed(DNS/DHCP) [false]
+    -o, --os OS                      p
+    -x, --ptable PTABLE              Partition Table ID [1]
+    -r COMPUTE-RESOURCE,             Compute Resource ID [1]
+        --compute-resource
+    -q, --quick                      Quick/quiet Mode, ie, do not check foreman/prmopt for valid values before submitting, don't output host details at the end [false]
+    -u, --start                      Start/Boot the vm after creation [true]
+    -w, --no-build                   Do not tell Foreman to build the host, only create
+```
+
+If *some* options are not specified (things we can ask Foreman about, like OS, media, arch, hostgroup, etc) and we are NOT in 'quick' mode, this script will query foreman and present a menu for you to use to select.
+
+Also, if not in quick mode, this script will check those same options against whats in foreman to pre-validate them (regardless if entered via cli, config, or prompt)
+
+Notes:
+------
+* For ```guest_type``` you must use the short/api name. These are available here: [[https://www.vmware.com/support/developer/converter-sdk/conv43_apireference/vim.vm.GuestOsDescriptor.GuestOsIdentifier.html]]
+* For ```path```, the full path must be used, typically: ```/Datacenters/<DATACENTER>/vm/<Your_Folders_Here>```
+* If you do not specify a mac address, it will be randomly generated for the system
+
+
+Examples:
+---------
+
+Quiet+Quick+Config file, prompting for password (would be sutable for scripting/looping for many vms):
+```
+mkvm.rb -p -q -f config_rc.yaml VM_Name
+```
--- mkvm.rb
+++ mkvm.rb
@@ -69,104 +69,192 @@ def passwd_prompt()
   return passwd
 end
 
+#simple boolean test if the name is already in foreman or not.
+def host_search(debug,name,passwd)
+  hammer_cmd = "hammer --output yaml"
+  if debug == true
+    hammer_cmd = hammer_cmd + " --debug"
+  end
+  if passwd !=nil
+    hammer_cmd = hammer_cmd + " --password #{passwd}"
+  end
+  hammer_cmd =  hammer_cmd + " host list --search #{name}"
+
+  list = YAML.load(`#{hammer_cmd}`)
+  if list == []
+    return true
+  else
+    return false
+  end
+end
+
+#craft the create host command
+def create_host_cmd(options,passwd)
+  hammer_cmd = "hammer"
+  if options[:debug] == true
+    hammer_cmd = hammer_cmd + " --debug"
+  end
+  if passwd !=nil
+    hammer_cmd = hammer_cmd + " --password #{passwd}"
+  end
+  hammer_cmd = hammer_cmd + " host create --name #{options[:name]}"
+  if options[:build] == true
+    hammer_cmd = hammer_cmd + " --build true"
+  else
+    hammer_cmd = hammer_cmd + " --build false"
+  end
+  hammer_cmd = hammer_cmd + " --hostgroup-id #{options[:host_group]}"
+  hammer_cmd = hammer_cmd + " --architecture-id #{options[:arch]}"
+  hammer_cmd = hammer_cmd + " --operatingsystem-id #{options[:os]}"
+  hammer_cmd = hammer_cmd + " --medium-id #{options[:media]}"
+  hammer_cmd = hammer_cmd + " --partition-table-id #{options[:ptable]}"
+  hammer_cmd = hammer_cmd + " --compute-resource-id #{options[:compute_resource]}"
+  hammer_cmd = hammer_cmd + " --compute-attributes guest_id='#{options[:guest_type]}',cpus=#{options[:cpus]},memory_mb=#{options[:memory]},cluster='#{options[:cluster]}',path='#{options[:path]}',start=#{options[:start]}"
+  if options[:nic_ip] != nil
+    hammer_cmd = hammer_cmd + " --ip #{options[:nic_ip]}"
+  end
+  if options[:nic_mac] != nil
+    hammer_cmd = hammer_cmd + " --mac #{options[:nic_mac]}"
+  end
+  hammer_cmd = hammer_cmd + " --interface=compute_type=#{options[:nic_type]},compute_network=#{options[:nic_network]},name=#{options[:name]},primary=true,identifier=#{options[:nic_name]},managed=#{options[:nic_managed]},provision=true"
+  hammer_cmd = hammer_cmd + " --volume datastore=#{options[:volume_datastore]},size_gb=#{options[:volume_size]},name=#{options[:name]}"
+
+  return hammer_cmd
+end
+
+def print_host_info(debug,name,passwd)
+  hammer_cmd = "hammer "
+  if debug == true
+    hammer_cmd = hammer_cmd + " --debug"
+  end
+  if passwd !=nil
+    hammer_cmd = hammer_cmd + " --password #{passwd}"
+  end
+  hammer_cmd = hammer_cmd + " host list --search #{name}"
+  out=`#{hammer_cmd}`
+  puts out
+end
+
+
 #Defaults for options.
 default_options = {
-  :verbose => false, # -v
-  :debug => false, #-d
-  :test => false, #-t
-  :passwd => false, #-p
-  :arch => 'x86_64',# -a
-  :host_group => nil, #-g
-  :media => nil, #-i
-  :os => nil, #-o
-  :ptable => nil, #-x
-  :compute_resource => nil, #-r
+  :verbose => false,
+  :debug => false,
+  :test => false,
+  :passwd => false,
+  :arch => '1',
+  :host_group => 1,
+  :media => nil,
+  :os => 1,
+  :ptable => 1,
+  :compute_resource => 1,
   :cpus => 1, #-c
-  :memory => 512, #-m
-  :guest_type => nil, #-k
-  :path => nil, #-e
-  :cluster => nil, #-b
-  :nic_type => 'VirtualE1000',#-s
-  :nic_network => nil, #-q
-  :nic_name => 'eth0', #-n
-  :nic_managed => false, #-l
-  :volume_datastore => nil, #-h
-  :volume_size => nil #-j
+  :memory => 512,
+  :guest_type => 'rhel7_64Guest',
+  :path => nil,
+  :cluster => nil,
+  :nic_type => 'VirtualE1000',
+  :nic_network => nil,
+  :nic_name => 'eth0',
+  :nic_managed => false,
+  :nic_ip => nil,
+  :nic_mac => nil,
+  :volume_datastore => nil,
+  :volume_size => 10,
+  :quick => false,
+  :start => true,
+  :build => true,
 }
 
 #Option Parsing
 options = default_options
 OptionParser.new do |opts|
-  opts.banner = "Usage: mkvm.rb [options]"
-  opts.on("-v", "--verbose", "Run verbosely") do
+  opts.banner = "Usage: mkvm.rb [options] NAME"
+  opts.on("-v", "--verbose", "Run verbosely [false]") do
     options[:verbose] = true
   end
-  opts.on("-d", "--debug", "Pass --debug to all hammer commands") do
+  opts.on("-d", "--debug", "Pass --debug to all hammer commands [false]") do
     options[:debug] = true
   end
-  opts.on("-f", "--configfile PATH", String, "Set config file (yaml, values will merge with defaults/cli options") do |path|
+  opts.on("-f", "--configfile PATH", String, "Set config file (yaml, values will merge with defaults/cli options [nil]") do |path|
     options.merge!(Hash[YAML::load(open(path)).map { |k, v| [k.to_sym, v] }])
   end
-  opts.on("-t", "--test", "Test, will not create vm, just output resulting hammer command") do
-    options[:debug] = true
+  opts.on("-t", "--test", "Test, will not create vm, just output resulting hammer command[false]") do
+    options[:test] = true
   end
-  opts.on("-p", "--passwd", "Prompt for password to pass to hammer with each call") do
+  opts.on("-p", "--passwd", "Prompt for password to pass to hammer with each call [false]") do
     options[:passwd] = true
   end
-  opts.on("-a", "--arch ARCH",String, "Architecture [x86_64]") do |arch|
+  opts.on("-a", "--arch ARCH",Integer, "Architecture ID [1]") do |arch|
     options[:arch] = arch
   end
-  opts.on("-c", "--cpus CPUS",Integer, "CPUS") do |cpus|
+  opts.on("-c", "--cpus CPUS",Integer, "CPUS [1]") do |cpus|
     options[:cpus] = cpus
   end
-  opts.on("-b", "--cluster CLUSTER",String,"VMware Cluster") do |clu|
+  opts.on("-b", "--cluster CLUSTER",String,"VMware Cluster [nil]") do |clu|
     options[:cluster] = clu
   end
-  opts.on("-e", "--path PATH",String, "Folder Path for VM") do |path|
+  opts.on("-e", "--path PATH",String, "Folder Path for VM [nil]") do |path|
     options[:path] = path
   end
   opts.on("-g", "--group HOST_GROUP",Integer, "Host GroupID [1]") do |group|
     options[:host_group] = group.to_i
   end
-  opts.on("-i", "--media MEDIA",Integer, "Installation MediaID ") do |media|
+  opts.on("-i", "--media MEDIA",Integer, "Installation MediaID [nil] ") do |media|
     options[:media] = media.to_i
   end
-  opts.on("-m", "--memory MEMORY",Integer, "Memory(RAM) in MB") do |mem|
+  opts.on("-m", "--memory MEMORY",Integer, "Memory(RAM) in MB [512]") do |mem|
     options[:memory] = mem.to_i
   end
-  opts.on("-n", "--nic-name ETH0",String, "NIC name [eth0]") do |nic_name|
+  opts.on( "--nic-name ETH0",String, "NIC name [eth0]") do |nic_name|
     options[:nic_name] = nic_name
   end
-  opts.on("-q", "--nic-network VLAN64",String, "Network/VLAN for NIC [VLAN01]") do |vlan|
+  opts.on( "--nic-network VLAN64",String, "Network/VLAN for NIC [VLAN01]") do |vlan|
     options[:nic_network] = vlan
   end
-  opts.on("-s", "--nic-type VirtualE1000",String, "NIC Hardware Type") do |nic_type|
+  opts.on( "--nic-type VirtualE1000",String, "NIC Hardware Type [VirtualE1000]") do |nic_type|
     options[:nic_type] = nic_type
   end
-  opts.on("-h", "--vol-datastore DATASTORE",String, "Datastore/LUN") do |ds|
+  opts.on( "--nic-ip 192.168.1.10",String, "NIC IP [nil]") do |nic_ip|
+    options[:nic_ip] = nic_ip
+  end
+  opts.on( "--nic-mac 00:32:32:32:32",String, "NIC Mac [random]") do |nic_mac|
+    options[:nic_mac] = nic_mac
+  end
+  opts.on( "--vol-datastore DATASTORE",String, "Datastore/LUN [nil]") do |ds|
     options[:volume_datastore] = ds
   end
-  opts.on("-j", "--vol-size SIZE_GB",Integer, "Volume size in GB") do |vol_size|
+  opts.on( "--vol-size SIZE_GB",Integer, "Volume size in GB [10]") do |vol_size|
     options[:volume_size] = vol_size
   end
-  opts.on("-k", "--guest_type TYPE",String, "Guest Type") do |guest|
+  opts.on("-k", "--guest_type TYPE",String, "Guest Type ['Red Hat Enterprise  Linux 7 (64-bit)']") do |guest|
     options[:guest_type] = guest
   end
-  opts.on("-l", "--nic_managed", "Set NIC to be foreman managed(DNS/DHCP, must provide MAC/IP, currently must be false)") do |guest|
-    options[:nic_managed] = guest
+  opts.on("-l", "--nic_managed", "Set NIC to be foreman managed(DNS/DHCP) [false]") do |managed|
+    options[:nic_managed] = managed
   end
-  opts.on("-o", "--os OS", Integer, "Operating System ID") do |os|
+  opts.on("-o", "--os OS", Integer, "Operating System ID" [1]) do |os|
     options[:os] = os.to_i
   end
-  opts.on("-x", "--ptable PTABLE",Integer, "Partition Table ID") do |ptable|
+  opts.on("-x", "--ptable PTABLE",Integer, "Partition Table ID [1]") do |ptable|
     options[:ptable] = p.to_i
   end
-  opts.on("-r", "--compute-resource COMPUTE-RESOURCE",Integer, "Compute Resource ID") do |r|
+  opts.on("-r", "--compute-resource COMPUTE-RESOURCE",Integer, "Compute Resource ID [1]") do |r|
     options[:compute] = r.to_i
   end
-
+  opts.on("-q", "--quick", "Quick/quiet Mode, ie, do not check foreman/prmopt for valid values before submitting, don't output host details at the end [false]") do |quick|
+    options[:quick] = quick
+  end
+  opts.on("-u", "--start", "Start/Boot the vm after creation [false]") do |start|
+    options[:start] = start
+  end
+  opts.on("-w", "--no-build", "Do not tell Foreman to build the host, only create ") do
+    options[:build] = false
+  end
 
 end.parse!
+options[:name] = ARGV.pop
+raise "Need to specify a hostname/VM name" unless options[:name]
 
 if options[:verbose] == true
   pp options
@@ -177,58 +265,87 @@ if options[:passwd] == true
 else
   passwd = nil
 end
-#get info to check/prompt if needed
-p "Getting Foreman info"
-os_list = get_foreman_list(options[:debug],"os",passwd)
-media_list = get_foreman_list(options[:debug],"medium",passwd)
-p_table_list= get_foreman_list(options[:debug],"partition-table",passwd)
-hg_list=get_foreman_list(options[:debug],"hostgroup",passwd)
-cr_list=get_foreman_list(options[:debug],"compute-resource",passwd)
-
-#Check empty options and give prompt/menu
-if options[:os] == nil
-  options[:os] = prompt_menu(os_list,['Id','Title'],'Operating System')
-end
-if options[:host_group] == nil
-  options[:host_group]=prompt_menu(hg_list,['Id','Title'],'Host Group')
-end
-if options[:media] == nil
-  options[:media]=prompt_menu(media_list,['Id','Name', 'Path'],'Operating System')
-end
-if options[:ptable] == nil
-  options[:ptable]=prompt_menu(p_table_list,['Id','Name', 'OS Family'],'Partition Table')
-end
-if options[:compute_resource] == nil
-  options[:compute_resource]=prompt_menu(cr_list,['Id','Name','Provider'],'Compute Resource')
-end
 
 
-#Check options exist in Foreman
-if os_list.select { |o| o['Id'] ==  options[:os]} == []
-  abort "I'm sorry, the OS id #{options[:os]} does not exist in foreman, exiting."
-end
-#options[:hostgroup]
-if hg_list.select { |h| h['Id'] ==  options[:host_group]} == []
-  abort "I'm sorry, the Host Group id #{options[:os]} does not exist in foreman, exiting."
-end
-#options[:media]
-if media_list.select { |m| m['Id'] ==  options[:media]} == []
-  abort "I'm sorry, the Installation Media id #{options[:os]} does not exist in foreman, exiting."
-end
-#options[:ptable]
-if p_table_list.select { |p| p['Id'] ==  options[:ptable]} == []
-  abort "I'm sorry, the Partition Table id #{options[:os]} does not exist in foreman, exiting."
-end
-#options[:compute_resource]
-if cr_list.select { |c| c['Id'] ==  options[:compute_resource]} == []
-  abort "I'm sorry, the compute_resource id #{options[:os]} does not exist in foreman, exiting."
+#If in quick mode, no need to fetch data/check it
+if options[:quick] == false
+  #get info to check/prompt if needed
+  puts "Getting Foreman info"
+  os_list = get_foreman_list(options[:debug],"os",passwd)
+  media_list = get_foreman_list(options[:debug],"medium",passwd)
+  p_table_list= get_foreman_list(options[:debug],"partition-table",passwd)
+  hg_list=get_foreman_list(options[:debug],"hostgroup",passwd)
+  cr_list=get_foreman_list(options[:debug],"compute-resource",passwd)
+  arch_list=get_foreman_list(options[:debug],'architecture',passwd)
+  #can do a search for hosts so we don't need ot get them all, plus we're not going to prompt for it with a menu.
+  #compute, interface, and volume attributes can't be checked...?
+
+  #Check empty options and give prompt/menu
+  if options[:arch] == nil
+    options[:arch] = prompt_menu(arch_list,['Id','Name'],'Arch')
+  end
+  if options[:os] == nil
+    options[:os] = prompt_menu(os_list,['Id','Title'],'Operating System')
+  end
+  if options[:host_group] == nil
+    options[:host_group]=prompt_menu(hg_list,['Id','Title'],'Host Group')
+  end
+  if options[:media] == nil
+    options[:media]=prompt_menu(media_list,['Id','Name', 'Path'],'Operating System')
+  end
+  if options[:ptable] == nil
+    options[:ptable]=prompt_menu(p_table_list,['Id','Name', 'OS Family'],'Partition Table')
+  end
+  if options[:compute_resource] == nil
+    options[:compute_resource]=prompt_menu(cr_list,['Id','Name','Provider'],'Compute Resource')
+  end
+
+  #Check options exist in Foreman
+  puts "Checking validity..."
+  #Check if name/hostname is taken already
+  if !host_search(options[:debug],options[:name],passwd)
+    abort "Host/VM name is already taken, please try again."
+  end
+
+  if arch_list.select { |a| a['Id'] ==  options[:arch]} == []
+    abort "I'm sorry, the Arch id #{options[:arch]} does not exist in foreman, exiting."
+  end
+  if os_list.select { |o| o['Id'] ==  options[:os]} == []
+    abort "I'm sorry, the OS id #{options[:os]} does not exist in foreman, exiting."
+  end
+  #options[:hostgroup]
+  if hg_list.select { |h| h['Id'] ==  options[:host_group]} == []
+    abort "I'm sorry, the Host Group id #{options[:os]} does not exist in foreman, exiting."
+  end
+  #options[:media]
+  if media_list.select { |m| m['Id'] ==  options[:media]} == []
+    abort "I'm sorry, the Installation Media id #{options[:os]} does not exist in foreman, exiting."
+  end
+  #options[:ptable]
+  if p_table_list.select { |p| p['Id'] ==  options[:ptable]} == []
+    abort "I'm sorry, the Partition Table id #{options[:os]} does not exist in foreman, exiting."
+  end
+  #options[:compute_resource]
+  if cr_list.select { |c| c['Id'] ==  options[:compute_resource]} == []
+    abort "I'm sorry, the compute_resource id #{options[:os]} does not exist in foreman, exiting."
+  end
 end
 
 #Choose a data store from the possible list.
 options[:volume_datastore] = pick_datastore(options[:volume_datastore])
-pp options[:volume_datastore]
 
+hammer_cmd = create_host_cmd(options,passwd)
 
+if options[:test] == true
+  puts "Test mode, I would have run the command:"
+  puts hammer_cmd
+  abort "Exiting"
+else
+  puts "Making VM.."
+  `#{hammer_cmd}`
+end
 
-
-#hammer -d host create --architecture x86_64 --medium-id 8 --partition-table-id 55 --name mn174-test --hostgroup-id 1 --compute-resource-id 1 --compute-attributes cpus=1,memory_mb=512,cluster='MED RC Cluster 01',path='/Datacenters/HMSDATACENTER/vm/RC VMs (RC Support)/Dev' --interface=compute_type='VirtualE1000',compute_network='VLAN64',name=eth0,primary=true,identifier=eth0,managed=false,provision=true --volume datastore=RC-SANLUN01R6TK-INFINI,size=10,name=mn174-test
+if options[:quick] != true && options[:test] != true
+  #show host info
+  print_host_info(options[:debug],options[:name],passwd)
+end
